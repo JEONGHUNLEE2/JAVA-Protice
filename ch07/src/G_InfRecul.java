a71240cca8bec3113160e11c378586dcea781870
LEEJEONGHUN <72964872+JEONGHUNLEE2@users.noreply.github.com>
2020-10-25 21:29:39 +0900

    Update G_InfRecul.java

--- ch07/src/G_InfRecul.java
+++ ch07/src/G_InfRecul.java
@@ -4,7 +4,7 @@ public class G_InfRecul
 
 	public static void main(String[] args)
 	{
-		showHi(3);
+		showHi(5);
 	}
 
 	public static void showHi(int cnt) // void와 int차이 다시 정리해보기
@@ -12,8 +12,14 @@ public class G_InfRecul
 		System.out.println("Hi~ " + cnt);
 		
 		if(cnt == 1)
+		{
 			return;
-		//showHi(cnt--); -> 무한 호출. 실행하지 말것 
-		showHi(--cnt);
+		}
+		else
+		{
+		showHi(--cnt);		//showHi(cnt--); -> 무한 호출. 실행하지 말것  
+		}
 	}
 }
+
+Q.여기서 반복해주는 그런 명령이 어디있는건지..
